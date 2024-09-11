93edf07164562a68da383e71561e1b21043ab466
Pratham722007 <patadiyapratham98@gmail.com>
2024-09-11 18:12:07 +0530

    Add files via upload

---
+++ iop/lab3 and lab4/iop28.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+int num,sum,q;
+q=0;
+printf("enter any number ");
+scanf("%d",&num)
+while (/* condition */)
+{
+    num%10;
+    sum=(num*num*num) +( q*q*q);
+    
+}
+
+return 0;
+}
